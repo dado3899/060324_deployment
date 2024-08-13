40c0378723ea2fe853dba29a3e6783afa1fc2c5f
David Doan <daviddoan@Davids-MacBook-Pro.local>
2024-08-13 11:47:00 -0600

    develop finish

--- server/services.py
+++ server/services.py
@@ -16,7 +16,6 @@ app = Flask(
     template_folder='../client/dist'
 )
 
-print(os.getenv('postgres_url'))
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('postgres_url')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
